2d139f8a412a6fe0e7dfd91f76711b0bede3b80f
totti <253262275@qq.com>
2019-11-25 17:03:53 +0800

    update netty.puml

---
+++ src/main/java/com/totti/netty/first/FirstNettyServer.java
@@ -0,0 +1,49 @@
+package com.totti.netty.first;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class FirstNettyServer {
+    public static void main(String[] args) {
+        // 1.first need two event loop - boss, worker
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            // 2. create start-up class, for the ease of to manage channel
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            // 3.
+            serverBootstrap.group(bossGroup, workerGroup);
+            serverBootstrap.channel(NioServerSocketChannel.class);
+
+            // I found there are two options, so why?
+            // but childOption is that after acceptor accepted the channel
+            // For now I think the method of option() is use for bossGroup
+            // and the method of childOption is use for workerGroup
+            serverBootstrap.option(ChannelOption.SO_BACKLOG, 128);
+            serverBootstrap.childOption(ChannelOption.SO_KEEPALIVE, true);
+
+            serverBootstrap.childHandler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                protected void initChannel(SocketChannel ch) throws Exception {
+
+                }
+            });
+
+            ChannelFuture channelFuture = serverBootstrap.bind(9000).sync();
+            channelFuture.channel().closeFuture();
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
