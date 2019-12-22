46afbb8beceaba3d1edb7fd312b30c19e0d4510c
totti <253262275@qq.com>
2019-12-22 10:21:58 +0000

    创建dns解析服务器，并创建说明文档

--- src/main/java/com/totti/dns/DnsNettyServer.java
+++ src/main/java/com/totti/dns/DnsNettyServer.java
@@ -1,5 +1,6 @@
 package com.totti.dns;
 
+import io.netty.bootstrap.Bootstrap;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
@@ -15,18 +16,18 @@ import io.netty.handler.codec.dns.DatagramDnsResponseEncoder;
 public class DnsNettyServer {
     public static void main(String[] args) {
         try {
-            EventLoopGroup bossGroup = new NioEventLoopGroup(1);
             EventLoopGroup workGroup = new NioEventLoopGroup();
 
-            ServerBootstrap serverBootstrap = new ServerBootstrap();
-            serverBootstrap.group(bossGroup, workGroup)
-                    .option(ChannelOption.SO_BACKLOG, 128)
-                    .channel(NioServerSocketChannel.class)
-                    .childHandler(new ChannelInitializer<NioDatagramChannel>() {
+            Bootstrap serverBootstrap = new Bootstrap();
+            serverBootstrap.group(workGroup)
+                    .option(ChannelOption.SO_BROADCAST, true)
+                    .channel(NioDatagramChannel.class)
+                    .handler(new ChannelInitializer<NioDatagramChannel>() {
                         @Override
                         protected void initChannel(NioDatagramChannel ch) throws Exception {
                             ch.pipeline().addLast(new DatagramDnsQueryDecoder());
                             ch.pipeline().addLast(new DatagramDnsResponseEncoder());
+                            ch.pipeline().addLast(new DnsParser());
                         }
                     });
 
