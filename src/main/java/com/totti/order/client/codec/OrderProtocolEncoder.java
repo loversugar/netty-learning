4616c78c84a46443f265c9ba1ecb010e9ee94ce2
loversugar <253262275@qq.com>
2020-11-25 11:12:58 +0100

    fix a bug

--- src/main/java/com/totti/order/client/codec/OrderProtocolEncoder.java
+++ src/main/java/com/totti/order/client/codec/OrderProtocolEncoder.java
@@ -2,19 +2,19 @@ package com.totti.order.client.codec;
 
 import java.util.List;
 
-import com.totti.order.common.ResponseMessage;
+import com.totti.order.common.RequestMessage;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToMessageEncoder;
 
-public class OrderProtocolEncoder extends MessageToMessageEncoder<ResponseMessage> {
+public class OrderProtocolEncoder extends MessageToMessageEncoder<RequestMessage> {
 
     @Override
-    protected void encode(ChannelHandlerContext channelHandlerContext, ResponseMessage responseMessage,
-        List<Object> list) throws Exception {
+    protected void encode(ChannelHandlerContext channelHandlerContext, RequestMessage requestMessage, List<Object> list)
+        throws Exception {
         ByteBuf buffer = channelHandlerContext.alloc().buffer();
-        responseMessage.encode(buffer);
+        requestMessage.encode(buffer);
 
         list.add(buffer);
     }
